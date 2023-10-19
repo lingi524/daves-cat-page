8f05b7d37de17d86a232444fc305faaaad59c381
Linnéa Gistrand <linnea.gistrand@gmail.com>
2023-10-19 12:37:05 +0200

    Add types to it's own file

---
+++ app/types/types.ts
@@ -0,0 +1,20 @@
+export type CatProps = {
+  id: number;
+  name: string;
+  gender: string;
+  bio: string;
+  date: string;
+  month: string;
+  year: string;
+};
+export interface PopupProps {
+  setShowPopup: React.Dispatch<React.SetStateAction<boolean>>;
+  setCats: React.Dispatch<React.SetStateAction<CatProps[]>>;
+  cats: CatProps[] ;
+}
+
+export interface CatCardProps {
+  cat: CatProps;
+  cats: CatProps[];
+  setCats: (cats: CatProps[]) => void;
+}
